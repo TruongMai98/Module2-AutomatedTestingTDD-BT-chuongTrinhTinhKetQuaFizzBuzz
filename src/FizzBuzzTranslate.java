ff559432f58db3ff72d5f74cf5772e9d36aefdb3
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 02:34:59 +0700

    pass test nhap so nguyen > 0

--- src/FizzBuzzTranslate.java
+++ src/FizzBuzzTranslate.java
@@ -1,5 +1,8 @@
 public class FizzBuzzTranslate {
     public static String fizzBuzzTranslate(int n) {
+        if (n < 0) {
+            return "nhap so nguyen > 0";
+        }
         return null;
     }
 }
