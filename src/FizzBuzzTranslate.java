7a189b45ae9f9e3bf6c2bead2b3a43228ca1585d
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 02:36:25 +0700

    refactor test nhap so nguyen > 0

--- src/FizzBuzzTranslate.java
+++ src/FizzBuzzTranslate.java
@@ -1,6 +1,6 @@
 public class FizzBuzzTranslate {
-    public static String fizzBuzzTranslate(int n) {
-        if (n < 0) {
+    public static String fizzBuzzTranslate(int integer) {
+        if (integer < 0) {
             return "nhap so nguyen > 0";
         }
         return null;
