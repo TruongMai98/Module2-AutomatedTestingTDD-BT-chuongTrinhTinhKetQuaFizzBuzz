3defa97b0db6e2b4be7c8b1d24a9b2c3f5c1e314
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 07:37:41 +0700

    add test Fizz

--- src/FizzBuzzTranslateTest.java
+++ src/FizzBuzzTranslateTest.java
@@ -8,10 +8,20 @@ class FizzBuzzTranslateTest {
     @Test
     @DisplayName("nhap so nguyen > 0")
     void fizzBuzzTranslate0() {
-        int n = -1;
+        int integer = -1;
 
         String excepted = "nhap so nguyen > 0";
-        String result = FizzBuzzTranslate.fizzBuzzTranslate(n);
+        String result = FizzBuzzTranslate.fizzBuzzTranslate(integer);
+        assertEquals(excepted, result);
+    }
+
+    @Test
+    @DisplayName("hien thi Fizz")
+    void fizzBuzzTranslate1() {
+        int integer = 3;
+
+        String excepted = "Fizz";
+        String result = FizzBuzzTranslate.fizzBuzzTranslate(integer);
         assertEquals(excepted, result);
     }
 }
