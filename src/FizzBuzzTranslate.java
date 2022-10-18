9099bdaf1dca502863a1c9c5ef0f6316470c6922
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 09:33:43 +0700

    pass test so chua so 3

--- src/FizzBuzzTranslate.java
+++ src/FizzBuzzTranslate.java
@@ -2,12 +2,14 @@ public class FizzBuzzTranslate {
     public static String fizzBuzzTranslate(int integer) {
         boolean divisibleBy3 = integer % 3 == 0;
         boolean divisibleBy5 = integer % 5 == 0;
+
+
         if (integer < 0) {
             return "nhap so nguyen > 0";
         } else {
             if (divisibleBy3 && divisibleBy5) {
                 return "FizzBuzz";
-            } else if (divisibleBy3 ) {
+            } else if (divisibleBy3 || num3(integer) == 1) {
                 return "Fizz";
             } else if (divisibleBy5) {
                     return "Buzz";
@@ -15,5 +17,9 @@ public class FizzBuzzTranslate {
                 return String.valueOf(integer);
             }
         }
+    }
+    public static int num3(int integer) {
+        String str = String.valueOf(integer);
+        return str.indexOf("3");
     }
 }
