248788532d75fcea603d4ab562a4cd23249b3249
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 07:39:44 +0700

    pass test Fizz

--- src/FizzBuzzTranslate.java
+++ src/FizzBuzzTranslate.java
@@ -2,6 +2,10 @@ public class FizzBuzzTranslate {
     public static String fizzBuzzTranslate(int integer) {
         if (integer < 0) {
             return "nhap so nguyen > 0";
+        } else {
+            if (integer % 3 == 0) {
+                return "Fizz";
+            }
         }
         return null;
     }
