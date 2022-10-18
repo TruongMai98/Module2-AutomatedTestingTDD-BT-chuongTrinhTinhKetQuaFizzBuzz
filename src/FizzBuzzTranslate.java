9c39ad722274eaf3c5e3f4ac160142be90013c44
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 07:41:34 +0700

    refactor test Fizz

--- src/FizzBuzzTranslate.java
+++ src/FizzBuzzTranslate.java
@@ -1,9 +1,10 @@
 public class FizzBuzzTranslate {
     public static String fizzBuzzTranslate(int integer) {
+        boolean divisibleBy3 = integer % 3 == 0;
         if (integer < 0) {
             return "nhap so nguyen > 0";
         } else {
-            if (integer % 3 == 0) {
+            if (divisibleBy3) {
                 return "Fizz";
             }
         }
